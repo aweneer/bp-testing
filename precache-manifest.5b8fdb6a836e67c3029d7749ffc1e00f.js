d5be46694d4422dccab989d9e95ba5d4bbc5dd47
Jan Hlaváč <hlavaj28@fel.cvut.cz>
2020-01-06 00:24:50 +0100

    Updates

---
+++ precache-manifest.5b8fdb6a836e67c3029d7749ffc1e00f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9c4f08f0292d4830dae824543692d93",
+    "url": "/bp-testing/index.html"
+  },
+  {
+    "revision": "3a4c932a74d43688716b",
+    "url": "/bp-testing/static/css/main.7ab34d70.chunk.css"
+  },
+  {
+    "revision": "1a7eae1725d08ecbd33f",
+    "url": "/bp-testing/static/js/2.d8b0f657.chunk.js"
+  },
+  {
+    "revision": "3a4c932a74d43688716b",
+    "url": "/bp-testing/static/js/main.bef4050b.chunk.js"
+  },
+  {
+    "revision": "fa11ea2771517a0fa84c",
+    "url": "/bp-testing/static/js/runtime-main.6ec97889.js"
+  }
+]);
